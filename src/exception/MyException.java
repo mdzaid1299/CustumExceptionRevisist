f7397be5c78ee389ee938591d1b2249d53f9c4b9
HP <mdzaid1299@gmail.com>
2022-08-31 14:59:28 +0530

    initial commit

--- src/exception/MyException.java
+++ src/exception/MyException.java
@@ -1,5 +1,7 @@
 package exception;
 
+import com.example.demo.MyDemo;
+
 public class MyException extends Exception {
     public MyException(String message) {
         super(message);
@@ -8,3 +10,19 @@ public class MyException extends Exception {
         super();
     }
 }
+
+/*
+Create a custom exception class called MyException.
+
+We are going create a MyDemo class , which will contain a method called int myBalance() and
+one attribute called balance.
+
+myBalance method will return the balance, if balance is lower than 1000, it will throw MyException, with message low balance.
+
+form ImplMain we have to call this myBalance and handle the appropriate exception.
+ */
+
+
+        // System.out.println("my balance is = " + demo.myBalance());
+
+
